72d2ee7a68ee749d67d17bbbd91452f51e3d55ae
only丶炀 <1763645020@qq.com>
2022-01-18 22:18:17 +0800

    test(string): add case
    完成string模块下的所有方法case

--- test/string.test.ts
+++ test/string.test.ts
@@ -1,7 +1,87 @@
-import { getURLParameters } from '../src/string';
+import { getURLParameters, getCustomKebabCase, getCustomCamelCase } from '../src/string';
 
 test('get url params', () => {
   let url = 'http://www.baidu.com?a=1&b=2&c=3';
   const query = getURLParameters(url);
   expect(query).toEqual({ a: "1", b: "2", c: "3" });
 })
+
+test('CamelCase to kebab-case', () => {
+
+  let key = 'getElementById';
+
+  const resultKey = getCustomKebabCase(key);
+
+  expect( resultKey ).toBe('get-element-by-id');
+
+});
+
+test('CamelCase to under_score_case', () => {
+
+  let key = 'getElementById';
+
+  const resultKey = getCustomKebabCase(key, '_');
+
+  expect( resultKey ).toBe('get_element_by_id');
+
+});
+
+test('pascal to kebab-case', () => {
+
+  let key = 'GetElementById';
+
+  const resultKey = getCustomKebabCase(key);
+
+  expect( resultKey ).toBe('get-element-by-id');
+
+});
+
+test('CamelCase  to custom name', () => {
+
+  let key = 'getElementById';
+
+  const resultKey = getCustomKebabCase(key, '%');
+
+  expect( resultKey ).toBe('get%element%by%id');
+
+});
+
+test('pascal to under_score_case', () => {
+
+  let key = 'GetElementById';
+
+  const resultKey = getCustomKebabCase(key, '_');
+
+  expect( resultKey ).toBe('get_element_by_id');
+
+});
+
+test('kebab-case to CamelCase', () => {
+
+  let key = 'get-element-by-id';
+
+  const resultKey = getCustomCamelCase(key);
+
+  expect( resultKey ).toBe('getElementById');
+
+});
+
+test('under_score_case to CamelCase', () => {
+
+  let key = 'get_element_by_id';
+
+  const resultKey = getCustomCamelCase(key, '_');
+
+  expect( resultKey ).toBe('getElementById');
+
+});
+
+test('custom name to CamelCase', () => {
+
+  let key = 'get%element%by%id';
+
+  const resultKey = getCustomCamelCase(key, '%');
+
+  expect( resultKey ).toBe('getElementById');
+
+});
