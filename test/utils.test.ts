d9898ddecb96f8b99f25956fc854c336d771aed0
only丶炀 <1763645020@qq.com>
2022-02-23 21:41:31 +0800

    test(( utils ) add utils test case

---
+++ test/utils.test.ts
@@ -0,0 +1,32 @@
+import { digitUppercase,  getFileExtension, generateUUID } from '../src/utils';
+
+test('amount to uppercase', () => {
+
+    expect(digitUppercase( 100 )).toEqual('壹佰元整');
+});
+
+test('decimal amount to uppercase', () => {
+
+    expect(digitUppercase( 100.99 )).toEqual('壹佰元玖角玖分');
+});
+
+test ('get file ext name 1', () => {
+
+    const fileName = 'file.jpg';
+    expect( getFileExtension( fileName )).toBe('jpg');
+
+})
+
+test ('get file ext name 2', () => {
+
+    const fileName = 'file.JPG';
+    expect( getFileExtension( fileName )).toBe('jpg');
+
+})
+
+test('create uuid ', () => {
+
+    const uuid = generateUUID();
+
+    expect(uuid.length > 0 ).toBeTruthy();
+})
