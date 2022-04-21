25cb68f49ff1485d0dafb243db7b024881943beb
only丶炀 <1763645020@qq.com>
2022-04-21 22:44:05 +0800

    refactor(helper): 删除webp&数组相等的判断方法

--- src/helper/index.ts
+++ src/helper/index.ts
@@ -11,39 +11,3 @@ export const isObject = ( data:any ):boolean => Object.prototype.toString.call (
 export const isEmptyArray = ( data: any ):boolean => isArray ( data ) && !data.length;
 
 export const isEmptyObject = ( data:any ):boolean => isObject ( data ) ? !Object.keys ( data ).length : false;
-
-/**
- *
- * @desc 判断浏览器是否支持webP格式图片
- * @return {Boolean}
- */
-
-export function isSupportWebP (): boolean {
-
-  return !![].map && document.createElement ( 'canvas' ).toDataURL ( 'image/webp' ).
-    indexOf ( 'data:image/webp' ) === 0;
-
-}
-
-/**
- * @desc JS判断两个数组是否相等
- * @param { array } arr1
- * @param { array } arr2
- * @returns { boolean } 返回true 或 false
- */
-
-export function arrayEqual ( arr1:any[], arr2:any[] ): boolean {
-
-  if ( arr1 === arr2 ) return true;
-
-  if ( arr1.length !== arr2.length ) return false;
-
-  for ( let i = 0; i < arr1.length; ++ i ) {
-
-    if ( arr1[ i ] !== arr2[ i ] ) return false;
-
-  }
-
-  return true;
-
-}
