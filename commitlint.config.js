4fecbd0c4e2e68bbd9f830ac2fa2837eb2146c2c
only丶炀 <1763645020@qq.com>
2022-04-25 22:54:55 +0800

    chore(commitlint): commitlint rules update
    n

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,14 +4,13 @@ module.exports = {
   ],
   rules: {
     'type-enum': [ 2, 'always', [
-      'upd', 'ui', 'perf', 'feat', 'fix', 'refactor', 'docs', 'chore', 'style', 'revert',
-    ] ],
+      'upd', 'ui', 'perf', 'feat', 'fix', 'refactor', 'docs', 'chore', 'style', 'revert', 'ci' ] ],
     'type-case': [ 0 ],
     'type-empty': [ 0 ],
     'scope-empty': [ 0 ],
     'scope-case': [ 0 ],
     'subject-full-stop': [ 0, 'never' ],
     'subject-case': [ 0, 'never' ],
-    'header-max-length': [ 0, 'always', 72 ],
+    'header-max-length': [ 0, 'always', 80 ],
   },
 };
