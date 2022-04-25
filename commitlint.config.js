5908c76514c8b73baf9a1cf03d7b16bd1f4fdca3
only丶炀 <1763645020@qq.com>
2022-04-25 22:42:10 +0800

    chore(commit type): custom commitlint commit type

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,7 +4,7 @@ module.exports = {
   ],
   rules: {
     'type-enum': [ 2, 'always', [
-      'upd', 'feat', 'fix', 'refactor', 'docs', 'chore', 'style', 'revert',
+      'upd', 'ui', 'perf', 'feat', 'fix', 'refactor', 'docs', 'chore', 'style', 'revert',
     ] ],
     'type-case': [ 0 ],
     'type-empty': [ 0 ],
