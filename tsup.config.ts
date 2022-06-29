0a89614c39b82ff88298065157431f3a164369df
only丶炀 <1763645020@qq.com>
2022-06-29 23:28:23 +0800

    build: 📦  打包配置修改

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig ( {
-  entry: [ 'src/main.ts' ],
+  entry: [ 'src/index.ts' ],
+  format: [ 'esm', 'cjs' ],
+  legacyOutput: true,
   splitting: false,
+  dts:true,
   sourcemap: false,
   minify: true,
   clean: true,
