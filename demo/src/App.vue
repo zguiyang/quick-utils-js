a1d107ab904bca28bb6a7b18e636f22ea96560f0
only丶炀 <1763645020@qq.com>
2022-07-05 22:55:06 +0800

    chore:增加demo

---
+++ demo/src/App.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+import { randomColor } from 'quick-utils-js';
+
+const hexColor = ref<string>( randomColor () );
+
+const generateColor = () => {
+  hexColor.value = randomColor ();
+};
+
+
+</script>
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+
+  <p>{{ hexColor }}</p>
+
+  <button type="button" @click="generateColor">生成颜色</button>
+
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ demo/vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import * as path from  'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+      vue(),
+  ],
+  resolve: {
+    alias: {
+        'quick-utils-js':  path.resolve ('..', 'src', 'index.ts'),
+   },
+
+  }
+})
