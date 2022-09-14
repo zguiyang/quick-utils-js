0e09250c7acd96d237e4140ecabf2e32b1345528
only丶炀 <1763645020@qq.com>
2022-09-14 12:26:46 +0800

    fix: 🐛 紧急修复`webpack`等打包器低版本不支持`es2020`语法问题，打包产物改为`es5`

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,6 +4,7 @@ export default defineConfig ( {
   entry: [ 'src/index.ts' ],
   format: [ 'esm', 'cjs' ],
   legacyOutput: true,
+  target: 'es5',
   splitting: false,
   dts: true,
   sourcemap: false,
