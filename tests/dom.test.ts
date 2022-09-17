8b00e29b91756f82913dbf0e83d99cfc336108e7
only丶炀 <1763645020@qq.com>
2022-09-17 16:43:22 +0800

    test: ✅ 修复测试用例错误

--- tests/dom.test.ts
+++ tests/dom.test.ts
@@ -2,19 +2,24 @@
  * @vitest-environment jsdom
  */
 
-import { describe, it, expect } from 'vitest';
+import { describe, beforeEach, it, expect, vi } from 'vitest';
 
 import { setScrollTop, getScrollTop } from '../src';
-describe('dom api test', () => {
-    let body = document.body,
-        bodyHeight = getComputedStyle(body).getPropertyValue('height'),
-        top = 100;
 
-    body.style.height = '10000px';
+window.scrollTo = vi.fn( () => { });
 
-    setScrollTop( top );
+ describe ('dom scroll top api test', () => {
+     beforeEach( () => {
+         const box = document.createElement('div' );
+         box.setAttribute('id', 'box');
+         box.setAttribute('height', '1000px');
+         document.body.appendChild( box );
+         setScrollTop( 0 );
+     })
 
     it ('set scroll top', () => {
+        const box = document.getElementById('box');
+        expect(box).not.toBeNull();
         expect(getScrollTop()).toBe( 0 );
     });
 
